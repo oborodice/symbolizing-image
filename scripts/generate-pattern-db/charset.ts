ea3505f30d3ca3128dcebb1bd04eb25bd97fefd8
Kazuki Tobita <kazukiti201@gmail.com>
2026-07-26 21:15:14 +0900

    Add Siddham charset

--- scripts/generate-pattern-db/charset.ts
+++ scripts/generate-pattern-db/charset.ts
@@ -16,3 +16,6 @@ export const KATAKANA = range(0x30a0, 0x30ff)
 export const HALFWIDTH_KATAKANA = range(0xff61, 0xff9f)
 
 export const FULLWIDTH_ASCII = range(0xff01, 0xff5e)
+
+// U+115B6, U+115B7は未割当のため除外
+export const SIDDHAM = [...range(0x11580, 0x115b5), ...range(0x115b8, 0x115dd)]
--- scripts/generate-pattern-db/index.ts
+++ scripts/generate-pattern-db/index.ts
@@ -4,6 +4,7 @@ import {
   KATAKANA,
   HALFWIDTH_KATAKANA,
   FULLWIDTH_ASCII,
+  SIDDHAM,
 } from './charset.ts'
 
 console.log('ASCII:', ASCII.length)
@@ -11,16 +12,18 @@ console.log('Hiragana:', HIRAGANA.length)
 console.log('Katakana:', KATAKANA.length)
 console.log('Halfwidth Katakana:', HALFWIDTH_KATAKANA.length)
 console.log('Fullwidth ASCII:', FULLWIDTH_ASCII.length)
+console.log('Siddham:', SIDDHAM.length)
 console.log(
   'Total:',
   ASCII.length +
     HIRAGANA.length +
     KATAKANA.length +
     HALFWIDTH_KATAKANA.length +
-    FULLWIDTH_ASCII.length,
+    FULLWIDTH_ASCII.length +
+    SIDDHAM.length,
 )
 
-// TODO: 漢字（Unihan kIRG_JSource）・梵字・幽霊文字を追加
+// TODO: 漢字（Unihan kIRG_JSource）を追加
 // TODO: Noto Sans JP / Noto Sans Siddhamでオフスクリーンcanvasに1文字ずつ描画
 // TODO: getImageDataで二値化し、Uint32Arrayにビット詰め
 // TODO: バイナリファイルとして書き出す
