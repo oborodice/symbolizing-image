aa7012216aaa4a0a2f893f1d75517d1556ff96ca
Kazuki Tobita <kazukiti201@gmail.com>
2026-07-26 21:02:10 +0900

    Add hiragana, katakana, halfwidth katakana, fullwidth ASCII charsets

---
+++ scripts/generate-pattern-db/charset.ts
@@ -0,0 +1,18 @@
+function range(start: number, end: number): number[] {
+  const codePoints: number[] = []
+  for (let cp = start; cp <= end; cp++) {
+    codePoints.push(cp)
+  }
+  return codePoints
+}
+
+export const ASCII = range(0x0020, 0x007e)
+
+// U+3040, U+3097, U+3098は未割当のため除外
+export const HIRAGANA = [...range(0x3041, 0x3096), ...range(0x3099, 0x309f)]
+
+export const KATAKANA = range(0x30a0, 0x30ff)
+
+export const HALFWIDTH_KATAKANA = range(0xff61, 0xff9f)
+
+export const FULLWIDTH_ASCII = range(0xff01, 0xff5e)
--- scripts/generate-pattern-db/index.ts
+++ scripts/generate-pattern-db/index.ts
@@ -1,6 +1,26 @@
-console.log('generate-pattern-db')
+import {
+  ASCII,
+  HIRAGANA,
+  KATAKANA,
+  HALFWIDTH_KATAKANA,
+  FULLWIDTH_ASCII,
+} from './charset.ts'
 
-// TODO: charset.mdの候補文字一覧を読み込む
+console.log('ASCII:', ASCII.length)
+console.log('Hiragana:', HIRAGANA.length)
+console.log('Katakana:', KATAKANA.length)
+console.log('Halfwidth Katakana:', HALFWIDTH_KATAKANA.length)
+console.log('Fullwidth ASCII:', FULLWIDTH_ASCII.length)
+console.log(
+  'Total:',
+  ASCII.length +
+    HIRAGANA.length +
+    KATAKANA.length +
+    HALFWIDTH_KATAKANA.length +
+    FULLWIDTH_ASCII.length,
+)
+
+// TODO: 漢字（Unihan kIRG_JSource）・梵字・幽霊文字を追加
 // TODO: Noto Sans JP / Noto Sans Siddhamでオフスクリーンcanvasに1文字ずつ描画
 // TODO: getImageDataで二値化し、Uint32Arrayにビット詰め
 // TODO: バイナリファイルとして書き出す
